4ae72dfc15e1a27ab068946b98f8115eb9a35888
LucianoJavierCostaPeralta <luchinjeje@gmail.com>
2021-04-17 18:01:39 -0300

    Desafio 1 js coder

---
+++ Clase-1-12-04-2021/index.js
@@ -0,0 +1,21 @@
+//Ingresp de nombvre y apellido 
+
+let nombre = prompt('Ingrese su nombre ');
+let apellido = prompt('Ingrese su apellido');
+
+let nombre_apellido = nombre + " " + apellido ;
+
+alert(`Hola ${nombre_apellido}`);
+
+
+//Suma de Numeros 
+
+let n1 = 2 ;
+let n2 = 3 ;
+
+let r = n1 + n2 ;
+
+alert(`${n1} + ${n2} = ${r}`);
+
+
+
