67148e5df37a1da191674aa7554322aa747848f5
LucianoJavierCostaPeralta <luchinjeje@gmail.com>
2021-04-19 21:49:30 -0300

    Desafio 2 Condicionales

---
+++ Clase-2-19-4-2021/server.js
@@ -0,0 +1,14 @@
+let edad = parseInt(prompt('Ingrese su edad para determinar si ùede votar  '));
+
+if (edad === 18) {
+
+    alert(`🎇🎇¡¡Felicidades!! ya eres mayor de edad y puedes votar 🎇🎇`);
+    
+} else  if (edad > 18 ){
+
+    alert('Esta no es tu primera ves 👻 , ya nos estamos haciendo viejos ');
+    
+} else {
+    alert('Disculpa ☹️ , todavia eres menor de edad 🔞 ');
+}
+   
