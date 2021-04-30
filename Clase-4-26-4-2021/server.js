45931427195f3b9e2e8a6b5c0814a70c6d7743e8
LucianoJavierCostaPeralta <luchinjeje@gmail.com>
2021-04-29 21:41:31 -0300

    Desafio 4 Funciones V2

--- Clase-4-26-4-2021/server.js
+++ Clase-4-26-4-2021/server.js
@@ -1,53 +1,48 @@
 //Function constante flecha
-//Constante    
-const CANTIDAD_NOTAS = parseInt(prompt('Ingrese el numero de calificaciones a evaluar . Recuerde que la calificacion a ingresar debe estar em el rango de 0 a 10'));
-const NOTA_APROBATORIA = 6 ;
-const NOTA_COMPLICADA = 5 ;
-
-
+//Constante
+const NOTA_APROBATORIA = 6;
+const NOTA_COMPLICADA = 5;
 
 //Lets
-let nota , aprobados = 0 , desaprobados = 0 , complicados = 0 ; 
+let nota,
+  aprobados = 0,
+  desaprobados = 0,
+  complicados = 0;
 
-const condicional = ()=>{
-    if(nota >= NOTA_APROBATORIA){
-        aprobados++
-    } else if (nota == NOTA_COMPLICADA){
-        complicados++
+const CANTIDAD_NOTAS = parseInt(
+  prompt(
+    "Ingrese el numero de calificaciones a evaluar . Recuerde que la calificacion a ingresar debe estar em el rango de 0 a 10"
+  )
+);
 
-    }else{
-        desaprobados++
-    }
-}
+const condicional = () => {
+  if (nota >= NOTA_APROBATORIA) {
+    aprobados++;
+  } else if (nota == NOTA_COMPLICADA) {
+    complicados++;
+  } else {
+    desaprobados++;
+  }
+};
 
-const pedirMota = () => {
+const pedirNota = () => {
+  //Ciclo For
+  for (let index = 1; index <= CANTIDAD_NOTAS; index++) {
+    //Ciclo doWhile
+    do {
+      nota = parseInt(prompt(`Nota nro ${index} `));
+    } while (nota < 0 || nota > 10);
 
-    //Ciclo For
-    for (let index = 1; index <= CANTIDAD_NOTAS; index++) {
-        
-        //Ciclo doWhile
-        do {
+    //funcion condicional
+    condicional();
+  }
 
-            nota = parseInt(prompt(`Nota nro ${index} ` ));
-            
-        } while (nota < 0 || nota > 10);
-
-
-        //condicional multiple
-        condicional();
-
-        
-    }
-    
-    //Imprimiendo resultados
-    alert(`Alumnos 
-    Aprobados : ${aprobados}
-    Complidados : ${complicados}
-    Desaprobados : ${desaprobados}`);
-
-
-    
-}
+  //Imprimiendo resultados
+ return document.write(`Alumnos <br>
+    Aprobados : ${aprobados} <br>
+    Complidados : ${complicados} <br>
+    Desaprobados : ${desaprobados} <br>`);
+};
 
 //LLamada a la Funcion
-pedirMota();
+pedirNota();
